bb8e3de8c907a190b3d933c50306f155d07da8c6
elener <paulina.wojno@gmail.com>
2021-07-01 11:59:02 +0200

    First working chart.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import { LineChart, Line } from 'recharts';
+import {getPomiary} from "./services/pomiary";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <LineChart width={400} height={400} data={getPomiary()}>
+                <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+            </LineChart>
+        </div>
+    );
 }
 
 export default App;
