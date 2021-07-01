c9bbb8bcc0bb219851bcc0b9cc3f8a58edb5e708
elener <paulina.wojno@gmail.com>
2021-07-01 12:25:21 +0200

    New functions for the chart.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
-import { LineChart, Line } from 'recharts';
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts';
 import {getPomiary} from "./services/pomiary";
 
 
 function App() {
     return (
-        <div className="App">
-            <LineChart width={400} height={400} data={getPomiary()}>
-                <Line type="monotone" dataKey="uv" stroke="#8884d8" />
-            </LineChart>
-        </div>
+        <LineChart width={600} height={300} data={getPomiary()} margin={{
+            top: 15,
+            right: 20,
+            bottom: 5,
+            left: 0
+            }}
+        >
+            <Line type="monotone" dataKey="uv" stroke="#8084d8" />
+            <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+            <XAxis dataKey={"name"}/>
+            <YAxis/>
+            <Tooltip/>
+        </LineChart>
+
     );
 }
 
